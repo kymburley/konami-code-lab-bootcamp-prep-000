b3651f2bbf19b2fde4859f3a714cbcebfed56f34
Kimberly Mozzetti <kimberly.mozzetti@gmail.com>
2017-12-05 11:44:05 +0000

    automatically backed up by learn

--- konami_code.js
+++ konami_code.js
@@ -3,13 +3,14 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
 function init() {
   // Write your JavaScript code inside the init() function
 
-  const alphabet = [65, 66, 67];
+  const alphabet = [];
   let index = 0;
 
   function onKeyDownHandler(e) {
     const key = parseInt(e.detail || e.which);
 
-    if (key === alphabet[index]) {
+    if (key === code[index]) {
+      alphabet.push(key);
       index++;
 
       if (index === alphabet.length) {
